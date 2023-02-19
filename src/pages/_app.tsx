9470e1995ded1edf382097efdfc4d28e6a86872b
Ritik Ranjan <rritik772@outlook.com>
2023-02-19 20:31:16 +0530

    project init

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
 import '@/styles/globals.css'
+import { AppShell, MantineProvider } from '@mantine/core'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        colorScheme: 'light'
+      }}
+    >
+      <AppShell
+        padding='md'
+      >
+        <Component {...pageProps} />
+      </AppShell>
+    </MantineProvider>
+  )
 }
